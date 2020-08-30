f2a9d3cc625898e089c3631488f7d9c5428ae1c1
Meet Patel <70450861+Meetpatel-3250@users.noreply.github.com>
2020-08-30 16:55:37 -0400

    Update addressValidation.py

--- addressValidation.py
+++ addressValidation.py
@@ -1,7 +1,3 @@
-# I, Meet Patel, student number 000794612, certify that all code submitted
-# is my own work; that I have not copied it from any other source.
-# I also certify that I have not allowed my work to be copied by others.
-
 provList = ("AB", "BC", "MB", "NB", "NL", "NT", "NS", "NU", "ON", "PE", "QC", "SK", "YT")
 
 #function to validate postal code
